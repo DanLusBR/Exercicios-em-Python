be3e3c20abd742806d8e24e03746521f857d2506
DanLus1 <samu.nico837@gmail.com>
2026-03-14 11:52:29 -0300

    Alturas

---
+++ Alturas/Alturas.py
@@ -0,0 +1,35 @@
+## Alturas
+## Fazer um programa para ler um número inteiro N e depois os dados (nome, idade e altura) de N pessoas. Em seguida, mostrar a altura média das pessoas, mostrar a porcentagem de pessoas com menos de 16 anos e listar os nomes
+## dessas pessoas.
+
+n: int; nmenores: int
+alturatotal: float; alturamedia: float; percentualMenores: float;
+
+n = int(input("Quantas pessoas serao digitadas?: "))
+
+nomes: [str] = [0 for x in range(n)] # type: ignore
+idades: [int] = [0 for x in range(n)] # type: ignore
+alturas: [float] = [0 for x in range(n)] # type: ignore
+
+for i in range(n):
+	print(f"Dados da {i+1}a pessoa:")
+	nomes[i] = str(input("Nome: "))
+	idades[i] = int(input("Idade: "))
+	alturas[i] = float(input("Altura: "))
+
+nmenores = 0
+alturatotal = 0
+for i in range(n):
+	if idades[i] < 16:
+		nmenores = nmenores + 1
+		alturatotal = alturatotal + alturas[i]
+
+alturamedia = alturatotal / nmenores
+percentualMenores = (float(nmenores) / n) * 100.0
+
+print(f"\nAltura media = {alturamedia:.2f}")
+print(f"Pessoas com menos de 16 anos: {percentualMenores:.1f}%")
+
+for i in range(n):
+	if idades[i] < 16:
+		print(nomes[i])
