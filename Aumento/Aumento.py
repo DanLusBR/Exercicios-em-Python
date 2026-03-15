14b1832a6f3550afbf63b82df58aabb6072049aa
DanLus1 <samu.nico837@gmail.com>
2026-03-15 11:31:32 -0300

    Aumento

---
+++ Aumento/Aumento.py
@@ -0,0 +1,28 @@
+## Aumento
+## Faça um programa que leia o salário de um funcionário e calcule o valor do aumento e do novo salário.
+
+salario: float; novosalario: float; aumento: float
+porcentagem: int
+
+salario = float(input("Digite o salario da pessoa: "))
+
+if salario <= 1000.0:
+	porcentagem = 20
+	aumento = salario * porcentagem / 100
+	novosalario = salario + aumento
+elif salario <= 3000.0:
+	porcentagem = 15
+	aumento = salario * porcentagem / 100
+	novosalario = salario + aumento
+elif salario <= 8000.0:
+	porcentagem = 10
+	aumento = salario * porcentagem / 100
+	novosalario = salario + aumento
+else:
+	porcentagem = 5
+	aumento = salario * porcentagem / 100
+	novosalario = salario + aumento
+
+print(f"Novo salario = R$ {novosalario:.2f}")
+print(f"Aumento = R$ {aumento:.2f}")
+print(f"Porcentagem = {porcentagem} %")
