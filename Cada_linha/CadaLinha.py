08f92b2bb29820892fcea4e3e5bb72bdbd39578b
DanLus1 <samu.nico837@gmail.com>
2026-03-15 11:31:54 -0300

    Cada_linha

---
+++ Cada_linha/CadaLinha.py
@@ -0,0 +1,27 @@
+## Cada Linha
+## Faça um programa que leia uma matriz de ordem n e mostre o maior elemento de cada linha.
+
+n: int; maior: int;
+
+n = int(input("Qual a ordem da matriz?: "));
+
+matriz: [[int]] = [[0 for x in range(n)] for x in range(n)]  # type: ignore
+maiorlinha: int = []
+
+for i in range(n):
+	for j in range(n):
+		matriz[i][j] = int(input(f"Elemento [{i},{j}]: "))
+
+for i in range(n):
+	maior = matriz[i][0]
+	for j in range(1, n):
+		if maior < matriz[i][j]:
+			maior = matriz[i][j]
+
+	maiorlinha.append(maior)
+
+
+print("MAIOR ELEMENTO DE CADA LINHA:")
+
+for i in range(n):
+	print(maiorlinha[i])
