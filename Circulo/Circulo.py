3e5533cc821425397d376d8e51fcd0add91293e5
DanLus1 <samu.nico837@gmail.com>
2026-03-16 19:27:41 -0300

    Circulo

---
+++ Circulo/Circulo.py
@@ -0,0 +1,10 @@
+## Circulo
+## Faça um programa que leia o valor do raio de um círculo e calcule a área do círculo, mostrando o resultado.
+
+r: float; area: float
+
+r = float(input("Digite o valor do raio do circulo: "))
+
+area = 3.14159 * r * r
+
+print(f"AREA = {area:.3f}")
