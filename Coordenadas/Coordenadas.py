a22a4eefead8ec525f0db35164bfc30c51231f04
DanLus1 <samu.nico837@gmail.com>
2026-03-17 11:27:24 -0300

    Coordenadas

---
+++ Coordenadas/Coordenadas.py
@@ -0,0 +1,23 @@
+## Coordenadas
+## Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. A seguir, determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem 
+## (x = y = 0).
+
+x: float; y: float
+
+x = float(input("Valor de X: "))
+y = float(input("Valor de Y: "))
+
+if x == 0 and y == 0:
+	print("Origem")
+elif x == 0:
+	print("Eixo Y")
+elif y == 0:
+	print("Eixo X")
+elif x > 0 and y > 0:
+	print("Q1")
+elif x < 0 and y > 0:
+	print("Q2")
+elif x < 0 and y < 0:
+	print("Q3")
+else:
+	print("Q4")
